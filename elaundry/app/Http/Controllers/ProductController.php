a36944f8fcce2662dde6fed8d7c5f9813f1fd430
IrinSultana111 <80588800+IrinSultana111@users.noreply.github.com>
2021-05-08 14:53:03 +0600

    product controller
    product controller

---
+++ elaundry/app/Http/Controllers/ProductController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\DB;
+
+use Illuminate\Http\Request;
+use App\product;
+use App\cart;
+use App\order;
+ 
+class ProductController extends Controller
+{
+    
+    function index()
+    {
+        $data= product::all();
+        return view('product',['products'=>$data]);
+    }
+    function detail($id)
+    {
+        $data=product::find($id);
+         
+        return view('details',['product'=>$data]);
+    }
+     function addToCart(Request $req)
+    {
+        if($req->session()->has('user'))
+       {
+           $cart= new Cart;
+           $data=  $req->session()->get('user');
+         $cart->user_id= $data->id;
+          $cart->product_id=$req->product_id;
+         $cart->save();
+         return redirect ('/product');
+         
+           
+       
+        
+       }
+       else 
+       {
+           return redirect('/master');
+       }
+    }
+
+    
+    static function cartItem()
+    { 
+        $data= session()->get('user');
+          $userId=$data->id;
+            return Cart:: where ('user_id',$userId)->count();
+    }
+    
+    function cartlist()
+    {
+        $data=   session()->get('user');
+          $userId=$data->id;
+
+        $products= DB::table('cart')->join('products','cart.product_id','=','products.id')
+        ->where('cart.user_id',$userId)
+        ->select('products.*','cart.id as cart_id')
+        ->get();
+        return view('cartlist',['products'=>$products]);
+    }
+
+    function removecartitem($id)
+    {
+
+        Cart:: destroy($id);
+        return redirect('/cartlist');
+    }
+  
+
+    
+
+
+   
+
+}
