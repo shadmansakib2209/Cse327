feec5f8b41550b1a4b5bb7431b6c414e9e52005d
shadmansakib2209 <80583740+shadmansakib2209@users.noreply.github.com>
2021-05-08 14:17:58 +0600

    controller for user
    controller for user

---
+++ elaundry/app/Http/Controllers/UserController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+
+use App\user;
+
+class UserController extends Controller
+{
+    //
+    function login(Request $req)
+    {          
+          
+        $user= DB::table('users')->where('email', $req->email)->first();
+       if ($user && Hash:: check ($req->password,$user->password) )
+       {
+          $req->session()->put('user',$user);
+        
+          return redirect ('/product');
+           
+         
+       }
+       else 
+      
+           echo ' not mathced';
+      
+    }
+    
+
+    function signup()
+    {    
+        return view('signup');
+    }
+
+    function insertregi(Request $req)
+    {   
+       $user =new user;
+       $user->name=$req->name;
+       $user->email=$req->email;
+       $user->password=Hash::make($req->password);
+       $user->save();
+      
+       return redirect('/master');
+    }
+
+}
